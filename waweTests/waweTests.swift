1e0fe1bc01850d2fd1fee6f59ff0c78ee829588d
burningmannn <daniil24022002@gmail.com>
2026-02-26 18:28:38 +0500

    FixActionsCi

---
+++ waweTests/waweTests.swift
@@ -0,0 +1,16 @@
+//
+//  waweTests.swift
+//  waweTests
+//
+//  Created by burningmannn on 2/26/26.
+//
+
+import Testing
+
+struct waweTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
